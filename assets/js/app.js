4ed49cb34e3da46f915bd7a6ac2b55106c1cb0df
Jesus Magallon <j.magallon@oohmd.com>
2018-04-17 15:00:46 -0500

    Add vuejs

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '#convert-app',
+    data: {
+      message: 'Hello Vue!'
+    }
+});
+
+alert("Estoy aqui!");
--- main.js
+++ main.js
@@ -1,29 +1,37 @@
+var host = window.location.href;
 
 fallback.load({
-    global_css: ['assets/css/style.css'],
+    global_css: [host+"assets/css/style.css"],
+    global_js: [host+"assets/js/app.js"],
 
     bootstrap_css: [
-        "/node_modules/bootstrap/dist/css/bootstrap.min.css",
-        "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        host+"node_modules/bootstrap/dist/css/bootstrap.min.css",
+        //"//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
     ],
 
     bootstrap_js: [
-        "/node_modules/bootstrap/dist/js/bootstrap.min.js",
-        "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        host+"node_modules/bootstrap/dist/js/bootstrap.min.js",
+        //"//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
     ],
 
     jQuery: [
-        'node_modules/jquery/dist/jquery.slim.min.js',
-        '//code.jquery.com/jquery-3.2.1.slim.min.js'
+        host+"node_modules/jquery/dist/jquery.slim.min.js",
+        //'//code.jquery.com/jquery-3.2.1.slim.min.js'
     ],
+
+    vue: [
+        host+"node_modules/vue/dist/vue.min.js",
+        //'//cdn.jsdelivr.net/npm/vue@2.5.16/dist/vue.js'
+    ]
 }, {
     shim: {
-        'bootstrap_js': ['jQuery']
+        'bootstrap_js': ['jQuery'],
+        'global_js': ['vue']
     },
 
-    /*callback: function(success, failed) {
-        console.log(success, failed);
-    }*/
+    callback: function(success, failed) {
+    
+    }
 });
 
 fallback.ready(function() {
