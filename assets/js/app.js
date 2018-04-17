7623e1c05a284ab3058454aba8df3d58e504d5a6
Jesus Magallon <j.magallon@oohmd.com>
2018-04-17 15:45:30 -0500

    Add support to lower case and upper case

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,26 @@
 var app = new Vue({
     el: '#convert-app',
     data: {
-      message: 'Hello Vue!'
+      text: ''
+    },
+    computed: {
+      charactersCount() {
+          return this.text.length;
+      },
+      wordCount() {
+        if(this.text.length == 0) return 0;
+        return this.text.trim().replace(/\s+/gi, ' ').split(' ').length;
+      }
+    },
+    methods: {
+      clc: function(ev) {
+        this.text = this.text.toLowerCase().trim();
+      },
+      cuc: function(ev) {
+        this.text = this.text.toUpperCase().trim();
+      },
+      ccc: function(ev) {
+        this.text = this.text.toCapitalCase().trim();
+      }
     }
 });
