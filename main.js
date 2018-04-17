3216ff861c97d0fcf960085d16e6cc02ef41bf91
Jesus Magallon <j.magallon@oohmd.com>
2018-04-17 13:13:25 -0500

    Install and configure fallbackjs

---
+++ main.js
@@ -0,0 +1,31 @@
+
+fallback.load({
+    global_css: ['/assets/css/style.css'],
+
+    bootstrap_css: [
+        "/node_modules/bootstrap/dist/css/bootstrap.min.css",
+        "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    ],
+
+    bootstrap_js: [
+        "/node_modules/bootstrap/dist/js/bootstrap.min.js",
+        "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+    ],
+
+    jQuery: [
+        '/node_modules/jquery/dist/jquery.slim.min.js',
+        '//code.jquery.com/jquery-3.2.1.slim.min.js'
+    ],
+}, {
+    shim: {
+        'bootstrap_js': ['jQuery']
+    },
+
+    /*callback: function(success, failed) {
+        console.log(success, failed);
+    }*/
+});
+
+fallback.ready(function() {
+    console.log("All loaded!");
+});
